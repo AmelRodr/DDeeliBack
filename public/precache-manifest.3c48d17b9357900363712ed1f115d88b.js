34b67e7153a621bcb7ed9239103d571bcf2612ff
AmelRodr <amelrodr@gmail.com>
2018-10-15 15:56:47 -0500

    first herouke

---
+++ public/precache-manifest.3c48d17b9357900363712ed1f115d88b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac38b4aab22b10824578849c47d4fa54",
+    "url": "/static/media/2.ac38b4aa.jpg"
+  },
+  {
+    "revision": "40de5f8ff5b39352926d",
+    "url": "/static/css/main.b3957aa8.chunk.css"
+  },
+  {
+    "revision": "123716822216098549bef653678bf650",
+    "url": "/static/media/5.12371682.jpg"
+  },
+  {
+    "revision": "d868b0a8ec07e565e318",
+    "url": "/static/js/1.d868b0a8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40de5f8ff5b39352926d",
+    "url": "/static/js/main.40de5f8f.chunk.js"
+  },
+  {
+    "revision": "144b9d60a95997f7f58b98888c874241",
+    "url": "/static/media/3.144b9d60.jpg"
+  },
+  {
+    "revision": "ac786bc7f58987bbecec41671f0e6468",
+    "url": "/static/media/4.ac786bc7.jpg"
+  },
+  {
+    "revision": "ce65ccd5094c8966e0f85b8cd39b9c8a",
+    "url": "/static/media/1.ce65ccd5.jpg"
+  },
+  {
+    "revision": "d868b0a8ec07e565e318",
+    "url": "/static/css/1.85c90dc2.chunk.css"
+  },
+  {
+    "revision": "f5ccdd77182da1a9cfbb723d570167ed",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fd8a476fffff042ba2b812899c109ae.js"
+  "/precache-manifest.3c48d17b9357900363712ed1f115d88b.js"
 );
 
 workbox.clientsClaim();
